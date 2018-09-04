962da3fdb179d7af8ef8fa1e08983c0bf68d64da
vietrad <dangtham@tamviettech.vn>
2018-09-05 05:59:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Test\Test;
+
+echo Test::show();
---
+++ src/Test/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace Test;
+
+class Test
+{
+	public static function show(){
+		return "Hellworld Composer PHP";
+	}
+}
